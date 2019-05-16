fa3ae5fb2acf02fe9008bf9a797a5a55fc124aa8
Qorthony <ahmad.qorthony@gmail.com>
2019-05-16 08:57:53 +0700

    menambahkan data object

---
+++ data.js
@@ -0,0 +1,50 @@
+const kajian=[
+    {
+        id:1,
+        judul:"Pengajian Musawarah",
+        tanggal:"24/5/2019",
+        tempat:"Masjid Fathullah",
+        kota:"Tangerang Selatan",
+        pembicara:"Ustad Adi Hidayat"
+    },
+    {
+        id:2,
+        judul:"Sinau Bareng Caknun",
+        tanggal:"20/6/2019",
+        tempat:"Universitas Gadjah Mada",
+        kota:"Yogyakarta",
+        pembicara:"Emha Ainun Nadjib"
+    },
+    {
+        id:3,
+        judul:"Pengajian Rutin Mingguan",
+        tanggal:"30/7/2019",
+        tempat:"Masjid Istaqlal",
+        kota:"Jakarta Pusat",
+        pembicara:"Ustad Abdul Somad"
+    },
+    {
+        id:4,
+        judul:"Kajian Tafsir Mingguan",
+        tanggal:"30/8/2019",
+        tempat:"Ndalem Habib Novel",
+        kota:"Semarang",
+        pembicara:"Habib Novel Alaydrus"
+    },
+    {
+        id:5,
+        judul:"Shihab & shihab",
+        tanggal:"14/5/2019",
+        tempat:"Masjid Al-Jamiah UIN Ciputat",
+        kota:"Tangerang Selatan",
+        pembicara:"K.H. Quraish Shihab"
+    },
+    {
+        id:6,
+        judul:"Kajian Tafsir Jalalain",
+        tanggal:"13/7/2019",
+        tempat:"Pondok Persantren Al Munawwir",
+        kota:"Rembang",
+        pembicara:"K.H. Bahaudin Nur Salim"
+    }
+]
